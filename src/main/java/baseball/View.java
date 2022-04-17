0a2a2433b9947b18539750d4b6bc8ba12f0a6171
gokoy <gokoy50@gmail.com>
2022-04-17 23:44:44 +0900

    feat: ScoreBoard 클래스 출력 함수 생성

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,6 +1,11 @@
 package baseball;
 
 public class View {
+
+    public void inputNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
     public void gameOver() {
         System.out.print(GameConfig.COUNT + "개의 숫자를 모두 맞혔습니다! 게임종료");
 
@@ -8,5 +13,9 @@ public class View {
 
     public void restartGame() {
         System.out.print("게임을 새로 시작하려면 " + GameConfig.RESTART_GAME + ", 종료하려면 2를 입력하세요.");
+    }
+
+    public void printScore(ScoreBoard scoreBoard) {
+
     }
 }
