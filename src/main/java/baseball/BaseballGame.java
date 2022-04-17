c3b42d1d5aa74a948a7719568a889cc2c63e5fe9
gokoy <gokoy50@gmail.com>
2022-04-18 01:48:35 +0900

    feat: BaseBall 도메인 로직 생성
    - 객체 생성 시, 정답인 solution 변수 갖도록 생성
    - solution 변수 final로 정의해 변경하지 못하도록 함
    - BaseballGame.check(): 정답과 사용자의 입력 값을 비교
    - 인덱스와 값 모두 같으면 스트라이크
    - 인덱스는 다르고 값을 포함하면 볼
    - BaseballGame.isCorrectAnswer(): 정답을 맞췄는지 확인 후에 gameOver 플래그를 true로 변경

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,48 @@
 package baseball;
 
+import java.util.List;
+
 public class BaseballGame {
+
+    private final List<Integer> solution;
+
+    private boolean gameOver;
+
+    public BaseballGame(List<Integer> randomNums) {
+        solution = randomNums;
+        gameOver = false;
+    }
+
+    public ScoreBoard check(final List<Integer> answer) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < answer.size(); i++) {
+            strike += isStrike(answer.get(i), i);
+            ball += isBall(answer.get(i), i);
+        }
+
+        isCorrectAnswer(strike);
+
+        return new ScoreBoard(strike, ball);
+    }
+
+    private void isCorrectAnswer(int strike) {
+        if (strike == GameConfig.COUNT) {
+            gameOver = true;
+        }
+    }
+
+    private int isStrike(int answer, int idx) {
+        return answer == solution.get(idx) ? 1 : 0;
+    }
+
+    private int isBall(int answer, int idx) {
+        return solution.contains(answer) && solution.indexOf(answer) != idx ? 1 : 0;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
 
 }
