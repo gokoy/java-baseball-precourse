c04db9bd7f8a5b70c0a73122abeba9cab3e28d87
gokoy <gokoy50@gmail.com>
2022-04-17 22:40:31 +0900

    feat: BaseballGame 실행 및 종료
    - BaseballGame 객체를 생성하여 실행한다.
    - 해당 게임이 종료하면 view에서 종료, 재시작 구문을 출력한다.
    - 사용자 입력을 받아 1인 경우 BaseballGame 객체를 재생성하여 실행한다.

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,15 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Controller {
 
-    private View view;
-
-    private BaseballGame baseballGame;
+    private final View view;
 
     public Controller(View view) {
         this.view = view;
     }
 
     public void playGame() {
+        do {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.run();
+            view.gameOver();
+            view.restartGame();
+        } while (restart());
+    }
+
+    private boolean restart() {
+        String condition = Console.readLine();
+        return condition.equals("1");
     }
 }
