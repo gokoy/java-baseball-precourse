9101a41ab6793f80a7383c9a9c11ead8618c355e
gokoy <gokoy50@gmail.com>
2022-04-18 00:04:31 +0900

    refactor: 범위 제한 변경

--- src/main/java/baseball/GameConfig.java
+++ src/main/java/baseball/GameConfig.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class GameConfig {
 
-    public static final int START_BOUND_NUMBER = 0;
+    public static final int START_BOUND_NUMBER = 1;
     public static final int END_BOUND_NUMBER = 9;
     public static final int COUNT = 3;
 
