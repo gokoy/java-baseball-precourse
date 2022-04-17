98b50878e70cb9ff17694100ea2b03b5f522c530
gokoy <gokoy50@gmail.com>
2022-04-17 22:05:07 +0900

    feat: 게임 실행 요청을 받아들이는 함수 생성
    Controller.playGame() 함수

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -2,4 +2,14 @@ package baseball;
 
 public class Controller {
 
+    private View view;
+
+    private BaseballGame baseballGame;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public void playGame() {
+    }
 }
