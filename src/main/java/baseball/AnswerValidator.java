7d3cc76e11450c7b7e80c9fe1f39c0c31a859c88
gokoy <gokoy50@gmail.com>
2022-04-18 01:41:15 +0900

    refactor: if문 가독성 향상

--- src/main/java/baseball/AnswerValidator.java
+++ src/main/java/baseball/AnswerValidator.java
@@ -10,10 +10,12 @@ public class AnswerValidator {
     }
 
     public static void validate(String input) {
-        if (!(isThreeDigitsBetweenOneToNine(input) || isDifferentCharacter(input))) {
-            throw new IllegalArgumentException("The input must be " + GameConfig.COUNT + " different numbers between "
-                    + GameConfig.START_BOUND_NUMBER + " and " + GameConfig.END_BOUND_NUMBER + ".");
+        if (isThreeDigitsBetweenOneToNine(input) && isDifferentCharacter(input)) {
+            return;
         }
+
+        throw new IllegalArgumentException("The input must be " + GameConfig.COUNT + " different numbers between "
+                + GameConfig.START_BOUND_NUMBER + " and " + GameConfig.END_BOUND_NUMBER + ".");
     }
 
     private static boolean isThreeDigitsBetweenOneToNine(String str) {
