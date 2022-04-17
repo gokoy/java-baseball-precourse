537a713e1d4a7590fbe00d8d588b31cb28aa470b
gokoy <gokoy50@gmail.com>
2022-04-17 23:22:05 +0900

    feat: 게임 내에서 사용되는 상수 클래스 생성

---
+++ src/main/java/baseball/GameConfig.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class GameConfig {
+
+    public static final int START_BOUND_NUMBER = 0;
+    public static final int END_BOUND_NUMBER = 9;
+    public static final int COUNT = 3;
+
+    private GameConfig() {
+    }
+}
