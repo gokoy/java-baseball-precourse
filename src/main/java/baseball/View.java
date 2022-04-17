6c788903ad49a707fd23e1b67bdbc8f15452f749
gokoy <gokoy50@gmail.com>
2022-04-17 22:07:26 +0900

    feat: 게임 종료, 게임 재시작 출력용 함수 생성
    View.gameOver()
    View.restartGame()

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,5 +1,12 @@
 package baseball;
 
 public class View {
+    public void gameOver() {
+        System.out.print(GameConfig.COUNT + "개의 숫자를 모두 맞혔습니다! 게임종료");
 
+    }
+
+    public void restartGame() {
+        System.out.print("게임을 새로 시작하려면 " + GameConfig.RESTART_GAME + ", 종료하려면 2를 입력하세요.");
+    }
 }
