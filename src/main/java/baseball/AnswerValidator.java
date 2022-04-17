fdcb9e93b1c47da1798c0e110c3cb59583bd2391
gokoy <gokoy50@gmail.com>
2022-04-18 01:39:59 +0900

    feat: 사용자 입력값 검증 클래스 생성
    AnswerValidator.validate(): 사용자 입력값 조건을 검증
    AnswerValidator.isThreeDigitsBetweenOneToNine(): 1~9 사이의 3개의 숫자인지 검증(정규표현식 사용)
    AnswerValidator.isDifferentCharacter(): 서로다른 3개의 숫자인지 검증

---
+++ src/main/java/baseball/AnswerValidator.java
@@ -0,0 +1,33 @@
+package baseball;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class AnswerValidator {
+
+    private AnswerValidator() {
+    }
+
+    public static void validate(String input) {
+        if (!(isThreeDigitsBetweenOneToNine(input) || isDifferentCharacter(input))) {
+            throw new IllegalArgumentException("The input must be " + GameConfig.COUNT + " different numbers between "
+                    + GameConfig.START_BOUND_NUMBER + " and " + GameConfig.END_BOUND_NUMBER + ".");
+        }
+    }
+
+    private static boolean isThreeDigitsBetweenOneToNine(String str) {
+        return Pattern.matches(GameConfig.REGEX_CONDITION, str);
+    }
+
+    private static boolean isDifferentCharacter(String str) {
+        Set<Integer> check = new HashSet<>();
+
+        String[] strings = str.split("");
+        for (String string : strings) {
+            check.add(Integer.valueOf(string));
+        }
+
+        return check.size() == GameConfig.COUNT;
+    }
+}
